4db964e12f0410ebea78b92111c9cb1f2261922c
HanustaviraGA <68433367+HanustaviraGA@users.noreply.github.com>
2021-12-17 12:29:55 +0700

    Tambah

--- controller.php
+++ controller.php
@@ -4,17 +4,34 @@
     $data = $_REQUEST;
 
     switch($data['aksi']){
-        case 'tambah':
+        case 'tambah_pengaduan':
             if(isset($data['submit'])){
                 // Ambil input dari depan
                 $nama = $data['Nama'];
                 // Escape 
                 $esc_name = mysqli_real_escape_string($koneksi, $nama);
-                // Eksekusi 
-                $query = $koneksi->prepare("INSERT INTO pelaporan VALUES (?)");
-                $query->bind_param('s', $esc_name);
+                // Default
+                $unik = uniqid('PENGADUAN');
+                $pending = 'Pending';
+                // Eksekusi
+                $query = "INSERT INTO pelaporan(
+                    ID_Pelaporan, NIK, Tujuan, Keperluan, Keterangan, TanggalKejadian, Status)
+                    VALUES ('$unik', )";
+                $data = mysqli_query($koneksi, $query);
             }
             break;
+        case 'tambah_aspirasi':
+            if(isset($data['submit'])){
+                // Ambil input dari depan
+                $nama = $data['Nama'];
+                // Escape 
+                $esc_name = mysqli_real_escape_string($koneksi, $nama);
+                // Default
+                $unik = uniqid('ASPIRASI');
+                // Eksekusi 
+                
+            }
+            break;    
         default:
         echo 'Error';
     }
